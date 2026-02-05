fa47b8175e938a8a0938b08661b65fb755d3205e
UBAID-AKHTAR-243 <60124351+UBAID-AKHTAR-243@users.noreply.github.com>
2026-02-05 21:51:33 +0500

    Increase bottom margin of bot window frame

--- launcher.js
+++ launcher.js
@@ -27,7 +27,7 @@
             background: #111b21;
             border-radius:10px;
             box-shadow: 0 10px 40px rgba(0,0,0,0.4);
-            margin-bottom:10px;
+            margin-bottom:30px;
             overflow: hidden;
             border: 1px solid #222e35;
         }
